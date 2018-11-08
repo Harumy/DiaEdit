c2a97ec6efc4b197e01b602885af4f7bfc66282f
harmine <harumy.0321@gmail.com>
2018-11-08 17:46:25 +0900

    配列に格納する機能の完成(仮)

--- DiaEdit/Form1.cs
+++ DiaEdit/Form1.cs
@@ -17,15 +17,16 @@ namespace DiaEdit
         {
             InitializeComponent();
         }
-        string[] Station_List=new string[0];
-        string[] Train_List=new string[0];
+        string[][] Station_List=new string[0][];
+        string[][] Train_List=new string[0][];
+        int[][][] Data = new int[0][][];
         int Station_Length = 0;
         int Train_Length = 0;
         public void LoadDia()
         {
             OpenFileDialog OFD = new OpenFileDialog();
-            OFD.FileName = "List.txt";
-            OFD.InitialDirectory = @"M:\";
+            OFD.FileName = "大曲-盛岡.oud";
+            OFD.InitialDirectory = @"C:\Users\harum\Desktop";
             if (OFD.ShowDialog() == DialogResult.OK)
             {
                 Console.WriteLine(OFD.FileName);
@@ -45,6 +46,7 @@ namespace DiaEdit
                     case "Ressyasyubetsu.":
                         continue;
                     case "Dia.":
+                        DataEditStation();
                         continue;
                     case "Ressya.":
                         Train(sr);
@@ -59,6 +61,7 @@ namespace DiaEdit
             Console.WriteLine("Station");
             int a = Station_List.Length + 1;
             Array.Resize(ref Station_List, a);
+            Station_List[a - 1] = new string[2];
             bool A=true;
             while (A)
             {
@@ -67,6 +70,7 @@ namespace DiaEdit
                 {
                     //sr.ReadLine();
                     Station_Length++;
+                    Array.Resize(ref Data, Station_List.Length);
                     //Console.WriteLine("終わった");
                     A = false;
                 }
@@ -76,11 +80,12 @@ namespace DiaEdit
                     switch (read[0])
                     {
                         case "Ekimei":
-                            Station_List[Station_Length] = read[1];
-                            Console.WriteLine(Station_List[Station_Length]);
+                            Station_List[Station_Length][0] = read[1];
+                            continue;
+                        case "Ekikibo":
+                            Station_List[Station_Length][1] = read[1];
                             continue;
                         default:
-                            sr.ReadLine();
                             continue;
                     }
                 }
@@ -88,9 +93,14 @@ namespace DiaEdit
         }
         private void Train(StreamReader sr)
         {
+            Console.WriteLine("Train");
             int a = Train_List.Length + 1;
+            int b = a - 1;
+            string UD="";
             Array.Resize(ref Train_List, a);
-            bool A = true;
+            Train_List[b] = new string[6];//列車番号,列車名,号数,種別,備考,行先
+            bool A =new bool();
+            A = true;
             while (A)
             {
                 string reading = sr.ReadLine();
@@ -106,8 +116,26 @@ namespace DiaEdit
                     switch (read[0])
                     {
                         case "Ressyabangou":
-                            Train_List[Train_Length] = read[1];
-                            Console.WriteLine(Train_List[Train_Length]);
+                            Train_List[Train_Length][0] = read[1];
+                            Console.WriteLine(read[1]);
+                            continue;
+                        case "Ressyamei":
+                            Train_List[Train_Length][1] = read[1];
+                            continue;
+                        case "Gousuu":
+                            Train_List[Train_Length][2] = read[1];
+                            continue;
+                        case "Syubetsu":
+                            Train_List[Train_Length][3] = read[1];
+                            continue;
+                        case "Bikou":
+                            Train_List[Train_Length][4] = read[1];
+                            continue;
+                        case "EkiJikoku":
+                            EditDataArray(b,read[1],UD);
+                            continue;
+                        case "Houkou":
+                            UD = read[1];
                             continue;
                         default:
                             sr.ReadLine();
@@ -116,7 +144,150 @@ namespace DiaEdit
                 }
             }
         }
+        private void DataEditStation()
+        {
+            int length = Station_List.Length;
+            for (int i = 0; i < length; i++){
+                if(Station_List[i][1]== "Ekikibo_Syuyou")
+                {
+                    Data[i] = new int[2][];
+                    Data[i][0] = new int[0];
+                    Data[i][1] = new int[0];
+                }
+                else
+                {
+                    Data[i] = new int[1][];
+                    Data[i][0] = new int[0];
+                }
+            }
+        }
+        private void EditDataArray(int a,string dia,string Houkou)
+        {
+            Console.WriteLine(dia);
+            string[] tmp1 = dia.Split(new String[] { "," }, StringSplitOptions.None);
+            if (tmp1.Length != Station_List.Length)
+            {
+                Array.Resize(ref tmp1, Station_List.Length);
+            }
+            for(int j = 0; j < Station_List.Length; j++)
+            {
+                int i=j;
+                if (Houkou == "Nobori")
+                {
+                    i = Station_List.Length - 1 - j;
+                }
+                if (tmp1[i] != null)
+                {
+                    string[] tmp = tmp1[i].Split(';');
+                    if (tmp[0] == "")
+                    {
+                        if (Station_List[i][1] == "Ekikibo_Syuyou")
+                        {
+                            int length0 = Data[i][0].Length;
+                            int length1 = Data[i][1].Length;
+                            Array.Resize(ref Data[i][0], length0 + 1);
+                            Array.Resize(ref Data[i][1], length1 + 1);
+                        }
+                        else
+                        {
+                            int length0 = Data[i][0].Length;
+                            Array.Resize(ref Data[i][0], length0 + 1);
+                        }
+                    }
+                    else
+                    {
+                        switch (tmp[0])
+                        {
+                            case "1":
+                                string[] time = tmp;
+                                if (Station_List[i][1] == "Ekikibo_Syuyou")
+                                {
+                                    int length0 = Data[i][0].Length;
+                                    int length1 = Data[i][1].Length;
+                                    Array.Resize(ref Data[i][0], length0 + 1);
+                                    Array.Resize(ref Data[i][1], length1 + 1);
+                                    if (time[1].IndexOf('/') != -1)
+                                    {
+                                        time = time[1].Split('/');
+                                        if (time[0] != "")
+                                        {
+                                            Data[i][0][a] = int.Parse(time[0]);
+                                        }
+                                        else
+                                        {
+                                            Data[i][0][a] = 0;
+                                        }
+                                        if (time[1] != "")
+                                        {
+                                            Data[i][1][a] = int.Parse(time[1]);
+                                        }
+                                        else
+                                        {
+                                            Data[i][1][a] = 0;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Data[i][1][a] = int.Parse(time[1]);
+                                    }
+                                }
+                                else
+                                {
+                                    int length0 = Data[i][0].Length;
+                                    Array.Resize(ref Data[i][0], length0 + 1);
+                                    if (time[1].IndexOf('/') != -1)
+                                    {
+                                        time = time[1].Split('/');
 
+                                        if (time[1] != "")
+                                        {
+                                            Data[i][0][a] = int.Parse(time[1]);
+                                        }
+                                        else
+                                        {
+                                            Data[i][0][a] = 0;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Data[i][0][a] = int.Parse(time[1]);
+                                    }
+                                }
+                                continue;
+                            default:
+                                if (Station_List[i][1] == "Ekikibo_Syuyou")
+                                {
+                                    int length0 = Data[i][0].Length;
+                                    int length1 = Data[i][1].Length;
+                                    Array.Resize(ref Data[i][0], length0 + 1);
+                                    Array.Resize(ref Data[i][1], length1 + 1);
+                                }
+                                else
+                                {
+                                    int length0 = Data[i][0].Length;
+                                    Array.Resize(ref Data[i][0], length0 + 1);
+                                }
+                                continue;
+                        }
+                    }
+                }
+                else
+                {
+                    if (Station_List[i][1] == "Ekikibo_Syuyou")
+                    {
+                        int length0 = Data[i][0].Length;
+                        int length1 = Data[i][1].Length;
+                        Array.Resize(ref Data[i][0], length0 + 1);
+                        Array.Resize(ref Data[i][1], length1 + 1);
+                    }
+                    else
+                    {
+                        int length0 = Data[i][0].Length;
+                        Array.Resize(ref Data[i][0], length0 + 1);
+                    }
+                }
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             LoadDia();
